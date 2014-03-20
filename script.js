5a3422ba3b0753d3eaf1cfba057ee0439e05e95c
mikekavouras <mike@wantful.com>
2014-03-20 01:53:02 -0400

    v1

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
   var $header = $('#header');
+  var $header2 = $('header');
   var $spacer = $('#spacer');
+  var $body = $('.body');
+  var time = 800;
+  var method = 'easeInOutQuint';
+  var delay = 2000;
 
   setTimeout(function() {
-    $header.animate({'height': '154px'}, 600, 'easeInOutQuint', function() {
+    $body.show();
+    $header2.animate({ 'padding-top' : '0px' }, time, method);
+    $header.animate({ 'height' : '154px' }, time, method, function() {
       $header.css('position', 'relative');
       $spacer.remove();
     });
-  }, 2000);
+  }, delay);
 });
